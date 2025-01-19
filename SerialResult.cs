f2b1299a792ba101e9524f3efc1b3ffd9be71a7a
Stijn Wellink <wellinkstijn@gmail.com>
2025-01-19 18:12:09 +0100

    UNFINISHED
    pushes the data to the DB from deserialized JSON data coming from Arduino

---
+++ SerialResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialToDatabase
+{
+    public class SerialResult
+    {
+        public int TopLeft { get; set; } = 0;
+        public int TopRight { get; set; } = 0;
+        public int BottomLeft { get; set; } = 0;
+        public int BottomRight { get; set; } = 0;
+        public int ServoAttitude { get; set; } = 0;
+        public int ServoAzimuth { get; set; } = 0;
+
+
+        public override string ToString()
+        {
+            return $"--> TopLeft {TopLeft}, TopRight {TopRight}, BottomLeft {BottomLeft}, BottomRight {BottomRight}, ServoAttitude {ServoAttitude}, ServoAzimuth {ServoAzimuth}";
+
+        }
+    }
+}
